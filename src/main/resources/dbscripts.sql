95dea91480de7ce1429e5725baf0b381aad86842
camelia <taloscamelia@gmail.com>
2017-02-16 17:28:02 +0200

    step 7 created db

--- src/main/resources/dbscripts.sql
+++ src/main/resources/dbscripts.sql
@@ -1,16 +1,13 @@
-CREATE TABLE someTable (
-    idSomeTable bigserial NOT NULL PRIMARY KEY,
-    field1 character(20),
-    field2 int,
-    field3 DATE not null default CURRENT_DATE,
-    field4 numeric(7,2)
+CREATE TABLE recepies (
+    id bigserial NOT NULL PRIMARY KEY,
+    type character (20),
+    name character(20),
+    ingredients character (600),
+    instructions character (300),
+    duration character (30),
+    date DATE not null default CURRENT_DATE,
+
 );
 
-CREATE TABLE someOtherTable (
-    idSomeOtherTable bigserial NOT NULL PRIMARY KEY,
-    field1 character(20),
-    field2 int NOT NULL REFERENCES someTable(idSomeTable)
-);
 
-INSERT INTO sometable( field1, field2, field4) VALUES ('value1', 10, 5.2);
-INSERT INTO someOtherTable( field1, field2) VALUES ('value2', 1);
+INSERT INTO recipies ( name, ingredients, instructions, duration) VALUES ('Omleta in pita', '1 lingurita de ulei,2 linguri de ceapa tocata marunt, 1/4 ceasca de ardei rosu tocat marunt, 2 oua, sare, piper, 2 linguri branza feta maruntita, 2 linguri de humus' 'Se amesteca ingredientele (mai putin humusul) ca pentru omleta si se prajeste. Omleta se introduce in pita taiata partial pe jumatate', '30 minute', '2017-01-15');
